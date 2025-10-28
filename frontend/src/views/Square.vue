c8715e9b087ee0327100d9c242fb10aa8f5f3534
IsLing <wsk7931@163.com>
2025-10-28 22:42:37 +0800

    移除重构后重复代码

--- frontend/src/views/Square.vue
+++ frontend/src/views/Square.vue
@@ -2,19 +2,6 @@
   <div class="square-container">
     <NotesTopBar @search="onSearch" />
     <SquareBody :query="query" />
-    <div v-if="false">
-      <!-- 原广场内容已迁移到 SquareBody 中，保留备份块防止误删 -->
-      <header class="square-header">
-        <div class="brand">
-          <img src="https://api.iconify.design/mdi/notebook-outline.svg" alt="logo" width="28" height="28" />
-          <h1>云便签 · 广场</h1>
-        </div>
-        <div class="actions">
-          <el-button @click="$router.push('/login?redirect=%2F')">登录</el-button>
-          <el-button type="primary" @click="$router.push('/register')">注册</el-button>
-        </div>
-      </header>
-    </div>
   </div>
   
 </template>
